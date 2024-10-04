595c759c3d96e270ed5eb058ec52b2d480d100f4
Kamil <k.mirgasimov@innopolis.university>
2024-10-05 02:58:31 +0300

    calendar components

--- plant-care-ui-kit/dist/index.d.ts
+++ plant-care-ui-kit/dist/index.d.ts
@@ -3,3 +3,6 @@ export { default as GreenButton } from './components/Button';
 export { default as Grid } from './components/Grid';
 export { default as Text } from './components/Text';
 export { default as PlantSearch } from './components/PlantSearch';
+export { default as LargeDay } from './components/Calendar/DayCard/DayCard';
+export { default as SmallCalendar } from './components/Calendar/SmallGrid/SmallGrid';
+export { default as BigCalendar } from './components/Calendar/BigGrid/BigGrid';
